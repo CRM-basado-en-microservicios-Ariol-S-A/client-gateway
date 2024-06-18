7f59ecb484bd4a7674e97d9fbcff8f57c1ff3b2a
Mattias <duartemattias4@gmail.com>
2024-06-18 03:50:33 -0400

    add products conection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule } from './clients/clients.module';
 import { NatsModule } from './transports/nats.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ClientsModule, NatsModule],
+  imports: [ClientsModule, NatsModule, ProductsModule],
 })
 export class AppModule {}
